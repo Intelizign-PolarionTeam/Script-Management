4948883a3b52e6f06ab1cc50614a3a665f172145
12447 <12447@DENBG0415VM.IZD01.in>
2024-03-19 16:17:09 +0530

    Added Split Panel In Frontend And Create Popup Model

--- custom-script-management/webapp/script-manager/resources/script.js
+++ custom-script-management/webapp/script-manager/resources/script.js
@@ -10,25 +10,25 @@ require(['vs/editor/editor.main'], function() {
 		language: 'javascript',
 		theme: 'vs',
 		automaticLayout: true, // Ensures the editor layout adapts to changes in size
-        autoIndent: false
+		autoIndent: false
 	});
 
-	editor.onDidChangeModelContent(function(event) {
-	});
+	editor.onDidChangeModelContent(function(event) { });
 });
-
 
 var contextMenuOptions = {
 	selector: '.file-item',
 	callback: function(key, options) {
 		var filename = $(this).text();
+		console.log("Comming insdie rename..." + filename);
 		var dirname = $(this).closest('.file-group').data('heading');
 		if (key === 'delete') {
 			deleteFile(filename, dirname);
 		} else if (key === 'rename') {
-			var newfilename = prompt("Enter new file name:", fileName);
+
+			var newFileName = prompt("Enter new file name:", filename);
 			if (newFileName !== null && newFileName !== "") {
-				renameFile(filename, newfilename, dirname);
+				renameFile(filename, newFileName, dirname);
 			}
 		}
 	},
@@ -38,6 +38,24 @@ var contextMenuOptions = {
 	}
 };
 
+document.addEventListener('DOMContentLoaded', function() {
+	const bar = document.querySelector('.split__bar');
+	const left = document.querySelector('.split__left');
+	let mouseIsDown = false;
+
+	bar.addEventListener('mousedown', function(e) {
+		mouseIsDown = true;
+	});
+
+	document.addEventListener('mousemove', function(e) {
+		if (!mouseIsDown) return;
+		left.style.width = e.clientX + 'px';
+	});
+
+	document.addEventListener('mouseup', function() {
+		mouseIsDown = false;
+	});
+});
 
 $(document).ready(function() {
 	$.contextMenu(contextMenuOptions);
@@ -49,7 +67,7 @@ $(document).ready(function() {
 			const workItemHookMapObj = response.workItemHookMapObj;
 			const liveDocHookMapObj = response.liveDocHookMapObj;
 			const workFlowScriptMapObj = response.workFlowScriptMapObj;
-			
+
 
 			Object.keys(workItemHookMapObj).forEach(function(key) {
 				var workItemHookScriptName = workItemHookMapObj[key].jsName;
@@ -62,12 +80,13 @@ $(document).ready(function() {
 				var dirName = "documentsave";
 				$('#documentsave-file').append('<li class="file-item" data-heading="' + dirName + '">' + liveHookScriptName + '</li>');
 			});
-			
+
 			Object.keys(workFlowScriptMapObj).forEach(function(key) {
 				var workFlowScriptName = workFlowScriptMapObj[key].jsName;
 				var dirName = "scripts";
 				$('#workflow-file').append('<li class="file-item" data-heading="' + dirName + '">' + workFlowScriptName + '</li>');
 			});
+
 		},
 		error: function(error) {
 			console.error('Error occurred while fetching hookMapObj:', error);
@@ -78,118 +97,99 @@ $(document).ready(function() {
 var jsName;
 var dirName;
 $(document).on('click', '.file-item', function() {
-     jsName = $(this).text();
-     dirName = $(this).data('heading');
 
-    $.ajax({
-        url: `scriptmanager?action=getRespFileScriptContent&jsFileName=${jsName}&heading=${dirName}`,
-        type: 'GET',
-        dataType: 'json',
-        success: function(response) {
-            const hookScriptContent = response.hookScriptContent;
-            editor.setValue(hookScriptContent);
-        },
-        error: function(error) {
-            console.error('Error occurred while fetching script content:', error);
-        }
-    });
+	jsName = $(this).text();
+	dirName = $(this).data('heading');
+	console.log("dir name...." + dirName);
+	$.ajax({
+		url: `scriptmanager?action=getRespFileScriptContent&jsFileName=${jsName}&heading=${dirName}`,
+		type: 'GET',
+		dataType: 'json',
+		success: function(response) {
+			const hookScriptContent = response.hookScriptContent;
+			editor.setValue(hookScriptContent);
+		},
+		error: function(error) {
+			console.error('Error occurred while fetching script content:', error);
+		}
+	});
 });
 
 function saveHookScriptContent() {
-    var scriptContent = editor.getValue();
-    $.ajax({
-        url: 'scriptmanager?action=updatedScriptContent',
-        type: 'POST',
-        dataType: 'json',
-        data: {
-            hookScriptContent: scriptContent,
-            heading: dirName,
-            jsName: jsName
-        },
-        success: function(response) {
-			if(response){
-            console.log("Modified Script Updated to Specific Js File");
-            }
-        },
-        error: function(error) {
-            console.error('Error occurred while updating script content:', error);
-        }
-    });
+	var scriptContent = editor.getValue();
+	$.ajax({
+		url: 'scriptmanager?action=updatedScriptContent',
+		type: 'POST',
+		dataType: 'json',
+		data: {
+			hookScriptContent: scriptContent,
+			heading: dirName,
+			jsName: jsName
+		},
+		success: function(response) {
+			if (response) {
+				console.log("Modified Script Updated to Specific Js File");
+			}
+		},
+		error: function(error) {
+			console.error('Error occurred while updating script content:', error);
+		}
+	});
 }
 
 
-		function changeTheme(event) {
-			console.log(event);
-			var btn=event.target.checked;
-		    if (btn) {
-		        monaco.editor.setTheme("vs-dark");
-		    } else {
-		        // Toggle button is unchecked
-		        monaco.editor.setTheme("vs");
-		    }
-		}
-		var contextMenuOptions = {
-	    selector: '.file-item',
-	    callback: function(key, options) {
-		var filename = $(this).text();
-		var dirname = $(this).closest('.file-group').data('heading');
-		if (key === 'delete') {
-			deleteFile(filename, dirname);
-		} else if (key === 'rename') {
-			var newfilename = prompt("Enter new file name:", fileName);
-			if (newFileName !== null && newFileName !== "") {
-				renameFile(filename, newfilename, dirname);
-			}
-		}
-	},
-	items: {
-		delete: { name: "Delete" },
-		rename: { name: "Rename" }
+/*function changeTheme(event) {
+	console.log(event);
+	var btn = event.target.checked;
+	if (btn) {
+		monaco.editor.setTheme("vs-dark");
+	} else {
+		// Toggle button is unchecked
+		monaco.editor.setTheme("vs");
 	}
-};
+}*/
 
-              document.addEventListener('DOMContentLoaded', function() {
-            var summaries = document.querySelectorAll('.summary');
-            // Add click event listener to each summary element
-            summaries.forEach(function(summary) {
-                summary.addEventListener('click', function() {
-                    console.log('Clicked on summary:', this.textContent);
-                    // Remove 'active' class from all summaries
-                    summaries.forEach(function(summary) {
-                        summary.classList.remove('active');
-                        
-                    });
-                    // Add 'active' class to the clicked summary
-                    this.classList.add('active');
-                });
-            });
-        });
-        
-        $(document).ready(function() {
-	    $('.summary-icon').click(function() {
-			console.log('Plus icon clicked!');
-		var dirName = $(this).closest('.file-group').data('heading')
-		console.log("Directory Name is", dirName);
-		var newFileItem = $('<div class="file-item"><input type="text" class="filename-input" placeholder="Enter file name"></div>');
-		$(this).closest('.file-group').find('.file-list').append(newFileItem);
-		var filenameInput = newFileItem.find('.filename-input');
-		filenameInput.focus();
-
-		filenameInput.keypress(function(event) {
-			if (event.which === 13) {
-				var filename = $(this).val();
-				if (isValidFilename(filename)) {
-					saveFile(filename, dirName);
-				} else {
-					alert("Invalid filename. Only filenames ending with '.js' are allowed.");
-				}
-				saveFile(filename, dirName);
-				$(this).blur();
-			}
+document.addEventListener('DOMContentLoaded', function() {
+	var summaries = document.querySelectorAll('.summary');
+	summaries.forEach(function(summary) {
+		summary.addEventListener('click', function() {
+			summaries.forEach(function(summary) {
+				summary.classList.remove('active');
+			});
+			this.classList.add('active');
 		});
+	});
+});
 
-		filenameInput.blur(function() {
-		});
+$(document).ready(function() {
+	var directoryName;
+	// Bind click event to the "Create File" button
+	$('#popupContainer').addClass('hidden');
+	$('.summary-icon').click(function() {
+		directoryName = $(this).data('heading');
+		$('#popupContainer').removeClass('hidden');
+		$('#fileNameInput').focus();
+	});
+
+	// Bind click event to the "Create" button inside the popup
+	$('#popupContainer').on('click', '#createBtn', function() {
+		var filename = $('#fileNameInput').val().trim();
+		//dirName = $(this).data('heading');
+
+		if (isValidFilename(filename)) {
+			// var dirName = $('.file-group').data('heading');
+			saveFile(filename, directoryName);
+			alert("Your file is created Successfully in" + " " + directoryName + " folder.");
+			location.reload();
+			$('#popupContainer').addClass('hidden');
+		} else {
+			alert("Invalid filename. Only filenames ending with '.js' are allowed.");
+		}
+	});
+
+	// Bind click event to the "Close" button inside the popup
+	$('#popupContainer').on('click', '#closeBtn', function() {
+		$('#popupContainer').addClass('hidden');
 	});
 });
 
@@ -215,6 +215,7 @@ function saveFile(filename, dirname) {
 		}
 	});
 }
+
 function deleteFile(filename, dirname) {
 	$.ajax({
 		url: 'scriptmanager?action=deleteFile',
@@ -233,6 +234,7 @@ function deleteFile(filename, dirname) {
 }
 
 function renameFile(existingfilename, newfilename, dirname) {
+	console.log("dir name is..." + dirname + existingfilename + newfilename);
 	$.ajax({
 		url: 'scriptmanager?action=renameFileName',
 		type: 'POST',
